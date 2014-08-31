5fc6d25153623e6fb6bc9c02f0271bcbe4eda990
Chad Miller <chadtmiller15@gmail.com>
2014-08-30 22:02:01 -0700

    Added reviews to the Product model.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,7 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :created_at, :updated_at
 
-  has_one :category, embed: :id
+  has_one :category, embed: :id, include: true
+
+  has_many :reviews, embed: :ids, include: true
 end
