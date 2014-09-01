8e42b8a9cfbbded5a398f93fc0bb098cd82f050a
Chad Miller <chadtmiller15@gmail.com>
2014-08-31 23:58:57 -0700

    Update serializers.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,6 +1,7 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :instagram_tag, :created_at, :updated_at
 
+  has_one :user, embed: :id, include: true
   has_one :category, embed: :id, include: true
 
   has_many :reviews, embed: :ids, include: true
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,6 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :body, :review_type, :created_at, :updated_at
 
+  has_one :user, embed: :id, include: true
   has_one :product, embed: :id
 end
